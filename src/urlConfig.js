fae2740cc6a66bf2b23c8df72043111517973b84
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-26 10:46:13 +0530

    urlConfig Update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,8 @@
-const baseUrl = "http://localhost:4000"
+const baseUrl = "https://bazaar-by-mayur-kamble.herokuapp.com"
 
 export const api = `${baseUrl}/api`
 
 export const generatePublicUrl = (fileName) => {
-    return `${baseUrl}/public/${fileName}`
+    // return `${baseUrl}/public/${fileName}`
+    return `${fileName}`
 }
